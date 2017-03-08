05baea17262c1e2064fff51e41c2033efbc962fc
Mark Frost <cheerful.mf@gmail.com>
2017-03-08 13:14:03 +0500

    add(module_2) task3

---
+++ module2/task3.rb
@@ -0,0 +1,8 @@
+def fibonacci(to, numbers=[0,1])
+  length_numbers = numbers.length
+  return numbers if length_numbers >= to
+  numbers << numbers[length_numbers - 1] + numbers[length_numbers - 2]
+  fibonacci to, numbers
+end
+
+p fibonacci(100)
