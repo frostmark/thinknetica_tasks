8e9a89e6fc83b004e15e28221f72aba63be1b1f2
Mark Frost <cheerful.mf@gmail.com>
2017-03-08 13:48:37 +0500

    fix

--- module2/task3(recursion).rb
+++ module2/task3(recursion).rb
@@ -4,7 +4,7 @@ def fibonacci(to, numbers=[0,1])
   length_numbers = numbers.length
   return numbers if length_numbers >= to
   numbers << numbers[length_numbers - 1] + numbers[length_numbers - 2]
-  return fibonacci(to, numbers)
+  fibonacci(to, numbers)
 end
 
 p fibonacci(100)
