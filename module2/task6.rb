d4280f57cc7e9d8b22b61279961929adce824c3a
Mark Frost <cheerful.mf@gmail.com>
2017-03-09 21:11:42 +0500

    module 2 complete task 6

---
+++ module2/task6.rb
@@ -0,0 +1,31 @@
+cart = {}
+
+loop do
+  print 'Введите название товара, либо команду "стоп" чтобы выйти: '
+  good_name = gets.chomp
+  break if good_name.downcase == 'стоп'
+
+  print 'Введите цену товара: '
+  good_price = gets.chomp.to_f
+
+  print 'Введите кол-во товара: '
+  good_quantity = gets.chomp.to_i
+
+
+  cart[good_name] = {
+    good_price: good_price,
+    good_quantity: good_quantity
+  }
+end
+
+total_price = 0
+
+cart.each do |k, v|
+  sum = v[:good_price] * v[:good_quantity]
+  puts "Название #{k}, цена за единицу #{v[:good_price]}, кол-во #{v[:good_quantity]} сумма: #{sum}"
+  total_price += sum
+end
+
+puts "=" * 40
+
+puts "Итого: #{total_price}"
