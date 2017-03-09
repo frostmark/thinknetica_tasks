229c809823345738049ef9d4cdf7a08f1337a6b9
Mark Frost <cheerful.mf@gmail.com>
2017-03-09 22:51:09 +0500

    fix

--- module2/task5.rb
+++ module2/task5.rb
@@ -1,6 +1,29 @@
 MONTHS = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-while true do
+month = nil
+day = nil
+year = nil
+
+def leap?(year)
+  year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)
+end
+
+def day_of_year(day, month, year)
+  return day if month ==  1
+
+  MONTHS.take(month - 1).reduce(:+) + day
+end
+
+loop do
+  print "Год: "
+  year = gets.chomp.to_i
+  if year < 1
+    puts "Год должен быть больше 1"
+    next
+  end
+
+  MONTHS[1] = 29 if leap? year
+
   print "Месяц: "
   month = gets.chomp.to_i
   unless month.between?(1, 12)
@@ -16,26 +39,9 @@ while true do
     next
   end
 
-  print "Год: "
-  year = gets.chomp.to_i
-  if year < 1
-    puts "Год должен быть больше 1"
-    next
-  end
-
   break
 end
 
-def leap?(year)
-  year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)
-end
 
-def day_of_year(day, month, year)
-  return day if month ==  1
-
-  MONTHS[1] = 29 if leap? year
-
-  MONTHS.take(month - 1).reduce(:+) + day
-end
 
 p day_of_year(day, month, year)
